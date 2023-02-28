2df5fc6e5912d7353c14f54905bf7c6a2e60317a
erinbedri <erinbedri@gmail.com>
2023-02-28 12:10:03 +0100

    updated stock details design

--- src/components/StockData.js
+++ src/components/StockData.js
@@ -35,51 +35,49 @@ export default function StockData({ symbol }) {
     return (
         <div>
             {stockData && (
-                <div className="card">
-                    <div className="card-body">
-                        <div className="row">
-                            <img src={stockData.logo} style={{ width: "100px" }} />
-                            <div className="col">
-                                <div>
-                                    <span className="fw-bold">Name: </span>
-                                    {stockData.name}
-                                </div>
-                                <div>
-                                    <span className="fw-bold">Country: </span>
-                                    {stockData.country}
-                                </div>
-                                <div>
-                                    <span className="fw-bold">Industry: </span>
-                                    {stockData.finnhubIndustry}
-                                </div>
+                <div className="p-4 shadow-sm bg-white">
+                    <div className="row">
+                        <img src={stockData.logo} style={{ width: "100px" }} />
+                        <div className="col">
+                            <div>
+                                <span className="fw-bold">Name: </span>
+                                {stockData.name}
                             </div>
-                            <div className="col">
-                                <div>
-                                    <span className="fw-bold">IPO: </span>
-                                    {stockData.ipo}
-                                </div>
-                                <div>
-                                    <span className="fw-bold">Exchange: </span>
-                                    {stockData.exchange}
-                                </div>
-                                <div>
-                                    <span className="fw-bold">Currency: </span>
-                                    {stockData.currency}
-                                </div>
+                            <div>
+                                <span className="fw-bold">Country: </span>
+                                {stockData.country}
                             </div>
-                            <div className="col">
-                                <div>
-                                    <span className="fw-bold">Market Cap: </span>
-                                    {formatMarketCap(stockData.marketCapitalization)}
-                                </div>
-                                <div>
-                                    <span className="fw-bold">Shares Outstanding: </span>
-                                    {formatSharesOut(stockData.shareOutstanding)}
-                                </div>
-                                <div>
-                                    <span className="fw-bold">Homepage: </span>
-                                    {stockData.weburl}
-                                </div>
+                            <div>
+                                <span className="fw-bold">Industry: </span>
+                                {stockData.finnhubIndustry}
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div>
+                                <span className="fw-bold">IPO: </span>
+                                {stockData.ipo}
+                            </div>
+                            <div>
+                                <span className="fw-bold">Exchange: </span>
+                                {stockData.exchange}
+                            </div>
+                            <div>
+                                <span className="fw-bold">Currency: </span>
+                                {stockData.currency}
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div>
+                                <span className="fw-bold">Market Cap: </span>
+                                {formatMarketCap(stockData.marketCapitalization)}
+                            </div>
+                            <div>
+                                <span className="fw-bold">Shares Outstanding: </span>
+                                {formatSharesOut(stockData.shareOutstanding)}
+                            </div>
+                            <div>
+                                <span className="fw-bold">Homepage: </span>
+                                {stockData.weburl}
                             </div>
                         </div>
                     </div>
